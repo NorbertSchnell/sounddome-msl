18776a1f7e094af21b1087bc1f69c7fd62bc18af
Denise <denisealiciafranz@gmail.com>
2025-03-28 12:21:32 +0100

    copy http server, websocket server and first part of websocket listen code

--- sound2-projekt-soundscapes/server.js
+++ sound2-projekt-soundscapes/server.js
@@ -5,3 +5,53 @@ import https from 'https';
 import * as fs from 'fs';
 import * as Max from 'max-api';
 
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//stuff - norbert fragen!
+
+const key = fs.readFileSync('sslcert/selfsigned.key', 'utf8');
+const cert = fs.readFileSync('sslcert/selfsigned.crt', 'utf8');
+const credentials = { key, cert };
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//http server
+
+const httpPort = Number(process.env.PORT) || 3000;
+const app = express();
+
+const httpsServer = https
+    .createServer(credentials, app)
+    .listen(httpPort, () => console.log('HTTPS server listening on port ${httpPort}'));
+
+app.use(express.static('.'));
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//websocket server
+
+const webSocketServer = new WebSocket.Server({server: httpsServer});
+console.log('websocket server listening on port ${httpPort}');
+
+//listening to new web socket connections
+webSocketServer.on('connection', (socket, req) => { //as soon as new phone connects
+    const clientIndex = addClientToList(socket);
+    const clientId = clientIndex + 1; //set client ID
+
+    sendMessage(socket, ['client-id', clientId]);
+    Max.outlet('client-connect', clientId); //Max console print
+
+    const clientCount = getClientCount(); //how many clients are connected
+    Max.outlet('client-count', clientCount);
+
+    socket.on('message', (message) => {
+        const incoming = JSON.parse(message);
+        Max.outlet(incoming);
+        });
+
+    socket.on('close', () => {
+        if (removeClientFromList(socket) !== null) {
+            Max.outlet('client-disconnect', clientId);
+
+            const clientCount = getClientCount();
+            Max.outlet('client-count', clientCount);
+        }
+    });
+});
