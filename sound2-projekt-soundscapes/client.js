7fdc93addecf0484ba73c015b589d16ad58d1ee0
Denise <denisealiciafranz@gmail.com>
2025-03-28 14:46:31 +0100

    copied mobile control files instead

---
+++ sound2-projekt-soundscapes/client.js
@@ -0,0 +1,240 @@
+//global variables
+const messageElem = document.getElementById('message-display');
+const indexElem = document.getElementById('client-index');
+const canvas = document.getElementById('canvas');
+const context = canvas.getContext('2d');
+
+const minDistance = 1;
+const maxDistance = 5;
+
+let clientId = null;
+let azimuth = 0;
+let elevation = 0;
+let distance = 3;
+let refAlpha = null;
+
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+event listeners*/
+messageElem.innerText = 'touch screen to start';
+document.body.addEventListener('click', onClick);
+
+document.body.addEventListener('touchstart', onTouchStart);
+document.body.addEventListener('touchmove', onTouchMove);
+document.body.addEventListener('touchend', onTouchEnd);
+document.body.addEventListener('touchcancel', onTouchEnd);
+
+window.addEventListener('resize', updateCanvasSize);
+
+function updateCanvasSize() {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+}
+
+//touching the phone
+let touchId = null;
+let touchStartY = null;
+let startDistance = null;
+let lastTime = null;
+
+function onClick() {
+  const time = performance.now();
+
+  if (lastTime === null) {
+    init();
+  } else if (time - lastTime < 200) {
+    refAlpha = null;
+  }
+
+  lastTime = time;
+}
+
+function onTouchStart(e) { //when user starts to touch the screen
+  if (touchId === null) {
+    const touch = e.touches[0];
+    const id = touch.identifier;
+    const y = touch.pageY;
+
+    touchId = id;
+    touchStartY = y;
+    startDistance = distance;
+  }
+}
+
+function onTouchMove(e) { //user moves while touching
+  for (let touch of e.touches) {
+    if (touch.identifier === touchId) {
+      const y = touch.pageY;
+      const dY = touchStartY - y;
+      const dist = 2 * (maxDistance - minDistance) * dY / canvas.height;
+
+      distance = Math.max(minDistance, Math.min(maxDistance, startDistance + dist));
+      break;
+    }
+  }
+}
+
+function onTouchEnd(e) { 
+  touchId = null;
+}
+
+async function init() {
+  messageElem.innerText = '';
+
+  updateCanvasSize();
+
+  if (DeviceOrientationEvent) {
+    if (DeviceOrientationEvent.requestPermission) {
+      const permssion = await DeviceOrientationEvent.requestPermission();
+
+      if (permssion == "granted") {
+        start();
+      } else {
+        setErrorMessage("no permission for device orientation");
+      }
+    } else {
+      start();// no permission needed on non-iOS devices
+    }
+  } else {
+    setErrorMessage("device orientation not available");
+  }
+}
+
+function start() {
+  window.addEventListener("deviceorientation", onDeviceOrientation);
+  requestAnimationFrame(onAnimationFrame);
+}
+
+function setErrorMessage(text) {
+  messageElem.innerText = text;
+  messageElem.classList.add('error');
+}
+
+function onDeviceOrientation(e) {
+  if (refAlpha === null) {
+    refAlpha = e.alpha;
+  }
+
+  let alpha = e.alpha - refAlpha;
+  let beta = e.beta;
+
+  if (beta > 90) {
+    azimuth = alpha - 180;
+    elevation = 180 - beta;
+  } else if (beta < -90) {
+    azimuth = alpha - 180;
+    elevation = -beta - 180;
+  } else {
+    azimuth = alpha;
+    elevation = beta;
+  }
+
+  if (azimuth < -180) {
+    azimuth += 360;
+  } else if (azimuth >= 180) {
+    azimuth -= 360;
+  }
+
+  // paint stroke with normalized start and end coordinates and color
+  const outgoing = ['orientation', clientId, -azimuth, elevation, distance];
+
+  if (clientId !== null) {
+    const str = JSON.stringify(outgoing);
+    socket.send(str);
+  }
+}
+
+const minCircleSize = 20;
+const maxCircleSize = 120;
+
+function onAnimationFrame() {
+  const width = canvas.width;
+  const height = canvas.height;
+  const maxRadius = Math.min(width, height) / 2.7;
+  const normDist = (distance - minDistance) / (maxDistance - minDistance);
+  const circleSize = maxCircleSize - (maxCircleSize - minCircleSize) * normDist;
+
+  context.clearRect(0, 0, width, height);
+
+  const displayAzimuth = Math.PI * -azimuth / 180 + 0.5 * Math.PI;
+  const displayElevation = Math.PI * elevation / 180;
+  const radius = Math.abs(maxRadius * Math.cos(displayElevation));
+  const x = radius * Math.cos(displayAzimuth);
+  const y = radius * Math.sin(displayAzimuth);
+
+  const centerX = 0.5 * width - x;
+  const centerY = 0.5 * height - y;
+  const innerCircleRadius = Math.max(0, Math.abs(radius - 0.5 * circleSize));
+  const outerCircleRadius = Math.max(0, Math.abs(radius + 0.5 * circleSize));
+  const midCircleRadius = Math.max(0, radius);
+
+  if (elevation >= 0) {
+    context.fillStyle = '#0ff';
+    context.strokeStyle = '#0ff';
+  } else {
+    context.fillStyle = '#f0f';
+    context.strokeStyle = '#f0f';
+  }
+
+  context.globalAlpha = 1;
+  context.lineWidth = 0.5;
+  context.beginPath();
+  context.arc(0.5 * width, 0.5 * height, innerCircleRadius, 0, 2 * Math.PI);
+  context.moveTo(0.5 * width + outerCircleRadius, 0.5 * height);
+  context.arc(0.5 * width, 0.5 * height, outerCircleRadius, 0, 2 * Math.PI);
+  context.stroke();
+
+  context.globalAlpha = 0.2;
+  context.lineWidth = circleSize;
+  context.beginPath();
+  context.arc(0.5 * width, 0.5 * height, midCircleRadius, 0, 2 * Math.PI);
+  context.stroke();
+
+  context.globalAlpha = 1;
+  context.beginPath();
+  context.arc(centerX, centerY, 0.5 * circleSize, 0, 2 * Math.PI);
+  context.fill();
+
+  messageElem.innerHTML = `${Math.round(azimuth)} | ${Math.round(elevation)} | ${distance.toFixed(2)}`;
+
+  requestAnimationFrame(onAnimationFrame);
+}
+
+/****************************************************************
+ * websocket communication
+ */
+const webSocketPort = 3000;
+const webSocketAddr = window.location.hostname;
+
+const socket = new WebSocket(`wss://${webSocketAddr}:${webSocketPort}`);
+// const socket = new WebSocket(`ws://${webSocketAddr}:${webSocketPort}`);
+
+// listen to opening websocket connections
+socket.addEventListener('open', (event) => {
+});
+
+socket.addEventListener("close", (event) => {
+  clientId = null;
+  document.body.style.opacity = 0.333;
+});
+
+// listen to messages from server
+socket.addEventListener('message', (event) => {
+  const message = event.data;
+
+  if (message.length > 0) {
+    const incoming = JSON.parse(message);
+    const selector = incoming[0];
+
+    // dispatch incomming messages
+    switch (selector) {
+      case 'client-id':
+        clientId = incoming[1];
+        indexElem.innerHTML = clientId;
+        break;
+
+      default:
+        break;
+    }
+  }
+});
+
--- sound2-projekt-soundscapes/server.js
+++ sound2-projekt-soundscapes/server.js
@@ -1,57 +1,116 @@
-//needed modules
 import express from 'express';
 import WebSocket from 'ws';
 import https from 'https';
 import * as fs from 'fs';
 import * as Max from 'max-api';
 
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-//stuff - norbert fragen!
-
+// mkdir sslcert
+// openssl req -x509 -sha256 -nodes -days 365 -newkey rsa:2048 -keyout sslcert/selfsigned.key -out sslcert/selfsigned.crt
 const key = fs.readFileSync('sslcert/selfsigned.key', 'utf8');
 const cert = fs.readFileSync('sslcert/selfsigned.crt', 'utf8');
 const credentials = { key, cert };
 
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-//http server
-
+/****************************************************************
+ * http server
+ */
 const httpPort = Number(process.env.PORT) || 3000;
 const app = express();
 
 const httpsServer = https
-    .createServer(credentials, app)
-    .listen(httpPort, () => console.log('HTTPS server listening on port ${httpPort}'));
+  .createServer(credentials, app)
+  .listen(httpPort, () => console.log(`HTTPS server listening on port ${httpPort}`));
 
 app.use(express.static('.'));
 
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-//websocket server
+/****************************************************************
+ * websoket server
+ */
+const webSocketServer = new WebSocket.Server({ server: httpsServer });
+console.log(`websocket server listening on port ${httpPort}`);
 
-const webSocketServer = new WebSocket.Server({server: httpsServer});
-console.log('websocket server listening on port ${httpPort}');
+// listen to new web socket connections
+webSocketServer.on('connection', (socket, req) => {
+  const clientIndex = addClientToList(socket);
+  const clientId = clientIndex + 1;
 
-//listening to new web socket connections
-webSocketServer.on('connection', (socket, req) => { //as soon as new phone connects
-    const clientIndex = addClientToList(socket);
-    const clientId = clientIndex + 1; //set client ID
+  sendMessage(socket, ['client-id', clientId]);
+  Max.outlet('client-connect', clientId);
 
-    sendMessage(socket, ['client-id', clientId]);
-    Max.outlet('client-connect', clientId); //Max console print
+  const clientCount = getClientCount();
+  Max.outlet('client-count', clientCount);
 
-    const clientCount = getClientCount(); //how many clients are connected
-    Max.outlet('client-count', clientCount);
+  socket.on('message', (message) => {
+    const incoming = JSON.parse(message);
+    Max.outlet(incoming);
+  });
 
-    socket.on('message', (message) => {
-        const incoming = JSON.parse(message);
-        Max.outlet(incoming);
-        });
+  socket.on('close', () => {
+    if (removeClientFromList(socket) !== null) {
+      Max.outlet('client-disconnect', clientId);
 
-    socket.on('close', () => {
-        if (removeClientFromList(socket) !== null) {
-            Max.outlet('client-disconnect', clientId);
-
-            const clientCount = getClientCount();
-            Max.outlet('client-count', clientCount);
-        }
-    });
+      const clientCount = getClientCount();
+      Max.outlet('client-count', clientCount);
+    }
+  });
 });
+
+function sendMessage(socket, message) {
+  const str = JSON.stringify(message);
+  socket.send(str);
+}
+
+/****************************************************************
+ * clients
+ */
+const clientList = [];
+const freeClientIndices = new Set();
+const clientIndices = new Map();
+
+function getFreeClientIndex() {
+  const iter = freeClientIndices.values();
+  const first = iter.next();
+  const index = first.value;
+
+  if (index !== undefined) {
+    freeClientIndices.delete(index);
+    return index;
+  }
+
+  return clientList.length;
+}
+
+function addClientToList(socket) {
+  const clientIndex = getFreeClientIndex();
+
+  clientList[clientIndex] = socket;
+  clientIndices.set(socket, clientIndex);
+
+  return clientIndex;
+}
+
+function removeClientFromList(socket) {
+  const clientIndex = clientIndices.get(socket);
+
+  if (clientIndex !== undefined && clientList[clientIndex]) {
+    clientList[clientIndex] = null;
+    freeClientIndices.add(clientIndex);
+
+    // clear list
+    let lastIndex = clientList.length - 1;
+    while (lastIndex >= 0 && clientList[lastIndex] === null) {
+      freeClientIndices.delete(lastIndex);
+      clientList.length = lastIndex;
+      lastIndex--;
+    }
+
+    clientIndices.delete(socket);
+
+    return clientIndex;
+  }
+
+  return null;
+}
+
+function getClientCount() {
+  return clientIndices.size;
+}
